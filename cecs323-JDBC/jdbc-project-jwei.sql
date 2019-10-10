4f30351e6536f4d288ce95ed3a34adfbac5ae41d
Jessica Wei <jessicawei9@gmail.com>
2019-10-09 19:59:52 -0700

    laying out comment requirements

--- cecs323-JDBC/jdbc-project-jwei.sql
+++ cecs323-JDBC/jdbc-project-jwei.sql
@@ -1,5 +1,18 @@
 /**
  * Author:  Jessica Wei
  * Created: Oct 9, 2019
+ * Due: Oct 21, 2019
  */
+
+/* Creates new database  */
+CREATE JDBCProjectDB;
+
+/* Creates WritingGroup Table 
+    Holds the name of the group, the head writer, the founding year, and the subject the group writes about.*/
+
+/* Creates Publisher Table 
+    Holds the name, address, phone number, and email of the publisher.*/
+
+/* Creates Book Table 
+    Holds the title of the book, the year it was published, the number of pages it has, the author group name, and the publisher name.*/
 
